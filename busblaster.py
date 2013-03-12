99b24d500e28fe85a846efa46332cd4f17051981
Jason Harris <jason.harris@motorolasolutions.com>
2013-03-12 16:14:29 -0700

    The ft2232 jtag is working. Now able to read the CPLD idcode.

--- busblaster.py
+++ busblaster.py
@@ -3,6 +3,42 @@
 """
 Definitions and functions specific to the Bus Blaster v2.5 board
 See - http://www.seeedstudio.com/depot/bus-blaster-v2-jtag-debugger-p-807.html
+
+The bus blaster has a ft2232h with a CPLD connected to it. By reprogramming the CPLD
+you can adapt the board to an arbitrary pin out and a variety of voltage levels.
+
+FT2232H to XC2C32A Connectivity
+
+Channel A:
+This channel is connected to the IO pins of the CPLD and is used for normal operations.
+
+TCK    > AD0 > IO_31
+TDI    > AD1 > IO_30
+TDO    > AD2 > IO_29
+TMS    > AD3 > IO_28
+GPIOL0 > AD4 > IO_27
+GPIOL1 > AD5 > IO_26
+GPIOL2 > AD6 > IO_25
+GPIOL3 > AD7 > IO_24
+GPIOH0 > AC0 > IO_23
+GPIOH1 > AC1 > IO_22
+GPIOH2 > AC2 > IO_21
+GPIOH3 > AC3 > IO_20
+GPIOH4 > AC4 > IO_19
+GPIOH5 > AC5 > IO_18
+GPIOH6 > AC6 > IO_17
+GPIOH7 > AC7 > IO_16
+
+Channel B:
+This channel is connected to the JTAG pins of the CPLD and is used for programming the CPLD.
+
+TCK    > BD0 > TCK
+TDI    > BD1 > TDI
+TDO    > BD2 > TDO
+TMS    > BD3 > TMS
+GPIOL0..3 > Not connected
+GPIOH0..7 > Not connected
+
 """
 #-----------------------------------------------------------------------------
 
@@ -14,6 +50,9 @@ import ft2232
 
 _VID = 0x0403
 _PID = 0x6010
+
+_jtag_itf = 1 # external jtag is on the first interface
+_cpld_itf = 2 # cpld jtag is on the second interface
 
 #-----------------------------------------------------------------------------
 # cpld configuration files
@@ -27,7 +66,7 @@ class board:
     def __init__(self, itf = None):
         # TODO - itf - interface selection
         # setup the cpld access
-        chain = jtag.jtag(ft2232.jtag_driver(_VID, _PID, 1))
+        chain = jtag.jtag(ft2232.jtag_driver(_VID, _PID, _cpld_itf))
         chain.scan(jtag.IDCODE_XC2C32A)
         self.cpld = xc2c32a.xc2c32a(chain)
 
@@ -40,7 +79,7 @@ class board:
         s.append('BusBlaster v2.5')
         s.append(str(self.cpld))
         usercode = self.cpld.rd_usercode()
-        s.append('usercode: 0x%08x (%s)' % (usercode, _cpld_files.get(usercode, '??')))
+        s.append('usercode: 0x%08x (%s)' % (usercode, _cpld_files.get(usercode, '?')))
         return '\n'.join(s)
 
 #-----------------------------------------------------------------------------
--- ft2232.py
+++ ft2232.py
@@ -13,18 +13,47 @@ so I've only borrowed the ftdi specific portions.
 
 import time
 import array
+import sys
 from ftdi import Ftdi
 
 #------------------------------------------------------------------------------
 
-_TRST_TIME = 0.01
+_READ_RETRIES = 4
 
 #------------------------------------------------------------------------------
+# JTAG/GPIO Lines in MPSSE Mode
 
-_GPIO_TCK = (1 << 0) # output, normally low
-_GPIO_TDI = (1 << 1) # output, sampled on rising edge of tck
-_GPIO_TDO = (1 << 2) # input
-_GPIO_TMS = (1 << 3) # output, sampled on rising edge of tck
+_TCK    = (1 << 0) # output, normally low
+_TDI    = (1 << 1) # output, sampled on rising edge of tck
+_TDO    = (1 << 2) # input
+_TMS    = (1 << 3) # output, sampled on rising edge of tck
+_GPIOL0 = (1 << 4)
+_GPIOL1 = (1 << 5)
+_GPIOL2 = (1 << 6)
+_GPIOL3 = (1 << 7)
+_GPIOH0 = (1 << 8)
+_GPIOH1 = (1 << 9)
+_GPIOH2 = (1 << 10)
+_GPIOH3 = (1 << 11)
+_GPIOH4 = (1 << 12)
+_GPIOH5 = (1 << 13)
+_GPIOH6 = (1 << 14)
+_GPIOH7 = (1 << 15)
+
+# Commands in MPSSE Mode
+_MPSSE_WRITE_NEG = 0x01   # Write TDI/DO on negative TCK/SK edge
+_MPSSE_BITMODE   = 0x02   # Write bits, not bytes
+_MPSSE_READ_NEG  = 0x04   # Sample TDO/DI on negative TCK/SK edge
+_MPSSE_LSB       = 0x08   # LSB first
+_MPSSE_DO_WRITE  = 0x10   # Write TDI/DO
+_MPSSE_DO_READ   = 0x20   # Read TDO/DI
+_MPSSE_WRITE_TMS = 0x40   # Write TMS/CS
+
+#-----------------------------------------------------------------------------
+# MSB/LSB for 16 bit values
+
+def _msb(val): return (val >> 8) & 255
+def _lsb(val): return val & 255
 
 #-----------------------------------------------------------------------------
 # tms bit sequences
@@ -49,13 +78,57 @@ _sx2rti = _tms_bits('110')   # shift-x -> run-test/idle
 
 class jtag_driver:
 
-    def __init__(self, vid, pid, itf):
+    def __init__(self, vendor, product, interface):
         self.ftdi = Ftdi()
-        self.ftdi.open_mpsse(vid, pid, itf)
+        try:
+            self.ftdi.open_mpsse(vendor, product, interface)
+        except IOError as e:
+            print "ft2232 jtag driver: %s" % str(e)
+            self.ftdi = None
+            sys.exit(0)
         self.wrbuf = array.array('B')
+        # setup the gpio lines
+        self.gpio_dir = _TCK | _TDI | _TMS
+        self.gpio_val = 0
+        self.ftdi.write_data((Ftdi.SET_BITS_LOW, _lsb(self.gpio_val), _lsb(self.gpio_dir)))
+        self.ftdi.write_data((Ftdi.SET_BITS_HIGH, _msb(self.gpio_val), _msb(self.gpio_dir)))
+
+    def reset_jtag(self):
+        # TODO - pulse test reset line if we have one
+        self.state_idle()
 
     def __del__(self):
-        self.ftdi.close()
+        if self.ftdi:
+            self.ftdi.close()
+
+    def gpio_out(self, gpio):
+        if gpio <= _GPIOL3:
+            self.ftdi.write_data((Ftdi.SET_BITS_LOW, _lsb(self.gpio_val), _lsb(self.gpio_dir)))
+        else:
+            self.ftdi.write_data((Ftdi.SET_BITS_HIGH, _msb(self.gpio_val), _msb(self.gpio_dir)))
+
+    def gpio_set(self, gpio):
+        """set a gpio pin"""
+        self.gpio_dir |= gpio
+        self.gpio_val |= gpio
+        self.gpio_out(gpio)
+
+    def gpio_clr(self, gpio):
+        """clear a gpio pin"""
+        self.gpio_dir |= gpio
+        self.gpio_val &= ~gpio
+        self.gpio_out(gpio)
+
+    def gpio_rd(self, gpio):
+        """read a gpio pin"""
+        if gpio <= _GPIOL3:
+            self.ftdi.write_data((Ftdi.GET_BITS_LOW,))
+            val = self.ftdi.read_data_bytes(1, _READ_RETRIES)[0]
+        else:
+            self.ftdi.write_data((Ftdi.GET_BITS_HIGH,))
+            val = self.ftdi.read_data_bytes(1, _READ_RETRIES)[0]
+            val <<= 8
+        return (val & gpio) != 0
 
     def flush(self):
         """flush the write buffer to the ft2232"""
@@ -70,10 +143,77 @@ class jtag_driver:
             self.flush()
 
     def shift_tms(self, tms, flush = False):
-        self.write((Ftdi.WRITE_BITS_TMS_NVE, tms[0], tms[1]), flush)
+        cmd = _MPSSE_WRITE_TMS | _MPSSE_BITMODE | _MPSSE_LSB | _MPSSE_WRITE_NEG
+        self.write((cmd, tms[0], tms[1]), flush)
 
     def shift_data(self, tdi, tdo):
-        assert False
+        """
+        write (and possibly read) a bit stream from the JTAGkey
+        tdi - bit buffer of data to be written to the JTAG TDI pin
+        tdo - bit buffer for the data read from the JTAG TDO pin (optional)
+        """
+        wr = tdi.get()
+        io_bits = tdi.n - 1
+        io_bytes = io_bits >> 3
+        io_bits &= 0x07
+        last_bit = (wr[io_bytes] << (7 - io_bits)) & 128
+
+        if tdo is not None:
+            read_cmd = _MPSSE_DO_READ
+            read_len = io_bytes + 1
+            if io_bits:
+                read_len += 1
+        else:
+            read_cmd = 0
+            read_len = 0
+
+        # write out the full bytes
+        if io_bytes:
+            cmd = read_cmd | _MPSSE_DO_WRITE | _MPSSE_LSB | _MPSSE_WRITE_NEG
+            num = io_bytes - 1
+            self.write((cmd, _lsb(num), _msb(num)))
+            self.write(wr[0:io_bytes])
+
+        # write out the remaining bits
+        if io_bits:
+            cmd = read_cmd | _MPSSE_DO_WRITE | _MPSSE_LSB | _MPSSE_BITMODE | _MPSSE_WRITE_NEG
+            self.write((cmd, io_bits - 1, wr[io_bytes]))
+
+        # the last bit of output data is bit 7 of the tms value (goes onto tdi)
+        # continue to read during the return to run-test/idle to get the last bit of tdo data
+        cmd = read_cmd | _MPSSE_WRITE_TMS | _MPSSE_BITMODE | _MPSSE_LSB | _MPSSE_WRITE_NEG
+        self.write((cmd, _sx2rti[0], _sx2rti[1] | last_bit))
+
+        # if we are only writing, return
+        if tdo is None:
+            self.flush()
+            return
+
+        # make the ft2232 flush its data back to the PC
+        self.write((Ftdi.SEND_IMMEDIATE,), True)
+        rd = self.ftdi.read_data_bytes(read_len, _READ_RETRIES)
+
+        if io_bits:
+            # the n partial bits are in the top n bits of the byte
+            # move them down to the bottom
+            rd[-2] >>= (8 - io_bits)
+
+        # get the last bit from the tms response byte (last byte)
+        last_bit = (rd[-1] >> (7 - _sx2rti[0])) & 1
+        last_bit <<= io_bits
+
+        # add the last bit
+        if io_bits:
+            # drop the tms response byte
+            del rd[-1]
+            # or it onto the io_bits byte
+            rd[-1] |= last_bit
+        else:
+            # replace the tms response byte
+            rd[-1] = last_bit
+
+        # copy to the bit buffer
+        tdo.set(tdi.n, rd)
 
     def scan_ir(self, tdi, tdo = None):
         """write (and possibly read) a bit stream through the IR in the JTAG chain"""
@@ -87,16 +227,6 @@ class jtag_driver:
         self.shift_tms(_rti2sdr)
         self.shift_data(tdi, tdo)
 
-    def system_reset(self):
-        """assert the ~SRST line to reset the target"""
-        #TODO - pulse system reset line
-        pass
-
-    def test_reset(self, val):
-        """control the test reset line"""
-        #TODO - control the test reset line
-        pass
-
     def state_reset(self):
         """goto the reset state"""
         # any state -> reset
@@ -107,15 +237,8 @@ class jtag_driver:
         # any state -> run-test/idle
         self.shift_tms(_x2rti, True)
 
-    def reset_jtag(self):
-        """reset the TAP of all JTAG devices in the chain to the run-test/idle state"""
-        self.test_reset(True)
-        time.sleep(_TRST_TIME)
-        self.test_reset(False)
-        self.state_idle()
-
     def __str__(self):
         """return a string describing the device"""
-        return 'ft2232'
+        return self.ftdi.type
 
 #------------------------------------------------------------------------------
